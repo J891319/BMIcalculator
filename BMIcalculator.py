794a3b2f2d799619d9c43b36f326157f93186e01
James Deal <36328936+alphaEncryption@users.noreply.github.com>
2018-06-14 08:58:25 +0800

    Changed the variables so that they are floats instead of strings

--- BMIcalculator.py
+++ BMIcalculator.py
@@ -1,9 +1,9 @@
 name0 = input('What is the patient\'s name?')
-weight_kg0 = input('What is the weight of the patient? (kg)')
-height_m0 = input('What is the height of the patient? (m)')
+weight_kg0 = float(input('What is the weight of the patient? (kg)'))
+height_m0 = float(input('What is the height of the patient? (m)'))
 
 def bmi(name, weight_kg, height_m):
-    bmi = int(weight_kg) / (int(height_m) ** 2)
+    bmi = weight_kg / (height_m ** 2)
     print("bmi: ")
     print(bmi)
     if bmi < 25:
